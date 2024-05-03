3dfac099a82522c34dc15b8149944c2ad7356076
rqueztech <rqueztech@noreply.com>
2024-05-03 10:58:21 +0900

    Update csv operations

--- cmd/csvoperations.go
+++ cmd/csvoperations.go
@@ -1,42 +1,45 @@
 package main
 
 import (
+    "encoding/csv"
     "fmt"
     "os"
-    "encoding/csv"
 )
 
-func ReadCSV () ([]string, error) {
+func ReadCSV() ([]string, error) {
     var resourcesFile string = "./resources/RussianKanjiFrequencyList.csv"
 
     // Open the file
     file, err := os.Open(resourcesFile)
 
-    // Error in the code
+    // Error handling
     if err != nil {
         fmt.Println("File not found...")
         return nil, err
     }
 
-    // create a defer to close file until the function is finished
+    // Close the file at the end of the function
     defer file.Close()
-    
+
     var lines []string
 
-    // create a reader
+    // Create a reader
     reader := csv.NewReader(file)
 
-    // Read all records out of the reader
+    // Read all records from the CSV
     records, err := reader.ReadAll()
 
-    // do error check
+    // Error handling
     if err != nil {
         fmt.Println(err)
-    } 
+        return nil, err
+    }
 
+    // Append each record to lines
     for _, record := range records {
-        lines.append(lines, record)
+        lines = append(lines, record...)
     }
 
     return lines, nil
 }
+
