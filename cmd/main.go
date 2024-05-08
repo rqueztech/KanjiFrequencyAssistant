2337d45614e07732fd0c2c66dff835f994d8dc6a
rqueztech <rqueztech@noreply.com>
2024-05-08 09:40:41 +0900

    Update main function to read in map

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,42 @@
+// import the main package
 package main
 
 import (
-    "fmt";
+    "fmt" // import the fmt package for printing
+    "strings" // get the strings
 )
 
+// Main function
 func main() {
-    fmt.Println("Main Project")
+    // Prompt the user for input
+    fmt.Println("Spell out onyomi reading (hiragana, katakana, kanji): ")
+
+    // Read the CSV file and set to string
+    csv_as_string, err := ReadCSV()
+
+    // Check if there is an error
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    // create a csv string
+    var csv_row_sliced []string
+
+    kanjimap := make(map[string]string)
+
+    // range through the returned string
+    for _, csv_row := range(csv_as_string) {
+        // create a new slice which is going to contain your split
+        csv_row_sliced = strings.Split(csv_row, ",")
+
+        kanjimap[csv_row_sliced[0]] = csv_row_sliced[1]
+    }
+    
+    var name string
+
+    for {
+        fmt.Printf("Enter your name: ")
+        fmt.Scan(&name)
+        fmt.Println(kanjimap[name])
+    }
 }
