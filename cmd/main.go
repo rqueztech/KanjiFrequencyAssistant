0c76562251949a33525d39fb41e5691538d06b21
rqueztech <rqueztech@noreply.com>
2024-05-08 12:21:55 +0900

    Update main to recieve the hashmap and display.

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,6 @@ package main
 
 import (
     "fmt" // import the fmt package for printing
-    "strings" // get the strings
 )
 
 // Main function
@@ -12,31 +11,15 @@ func main() {
     fmt.Println("Spell out onyomi reading (hiragana, katakana, kanji): ")
 
     // Read the CSV file and set to string
-    csv_as_string, err := ReadCSV()
+    csv_as_map, err := ReadCSV()
 
     // Check if there is an error
     if err != nil {
         fmt.Println(err)
     }
-
-    // create a csv string
-    var csv_row_sliced []string
-
-    kanjimap := make(map[string]string)
-
-    // range through the returned string
-    for _, csv_row := range(csv_as_string) {
-        // create a new slice which is going to contain your split
-        csv_row_sliced = strings.Split(csv_row, ",")
-
-        kanjimap[csv_row_sliced[0]] = csv_row_sliced[1]
-    }
     
-    var name string
 
-    for {
-        fmt.Printf("Enter your name: ")
-        fmt.Scan(&name)
-        fmt.Println(kanjimap[name])
+    for index, currentValue := range(csv_as_map) {
+        fmt.Println(index, " :: ", currentValue)
     }
 }
