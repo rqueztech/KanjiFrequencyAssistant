c07debb030768db54c518bc0cbfdab9f4b324b3a
rqueztech <rqueztech@noreply.com>
2024-05-08 13:08:14 +0900

    Update main go with scan function, return matching kanji, and print out the count. NOTE: find out what is causing us to have to divide by 3 when used to get the right number, may have to do with csv duplicating

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,17 @@ package main
 
 import (
     "fmt" // import the fmt package for printing
+    "bufio" // import bufio to scan in user input
+    "os"
 )
 
 // Main function
 func main() {
     // Prompt the user for input
     fmt.Println("Spell out onyomi reading (hiragana, katakana, kanji): ")
+
+    scanner := bufio.NewScanner(os.Stdin)
+
 
     // Read the CSV file and set to string
     csv_as_map, err := ReadCSV()
@@ -18,8 +23,18 @@ func main() {
         fmt.Println(err)
     }
     
+    for {
+        scanner.Scan()
+        fmt.Print("Enter Input: ")
+        userInput := scanner.Text()
+        
 
-    for index, currentValue := range(csv_as_map) {
-        fmt.Println(index, " :: ", currentValue)
+        if val, ok := csv_as_map[userInput]; ok {
+            value := len(val) / 3
+            fmt.Println(val)
+            fmt.Printf("Number of occurences: %d\n", value)
+        } else {
+            fmt.Println("DOES NOT EXIST")
+        }
     }
 }
