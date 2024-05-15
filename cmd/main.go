2f48f86cc2e0df3351c47f733735005cd796b9dc
rqueztech <rqueztech@noreply.com>
2024-05-15 12:48:28 +0900

    Add concurrency to slash the load time importing four separate lists.

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,7 @@ import (
     "os/exec"
     "runtime"
     "net/url"
+    "sync"
 )
 
 func clearScreen() {
@@ -58,24 +59,55 @@ func main() {
     var wg sync.WaitGroup
 
     // create four different maps of type map[string][]rune. these will return the map returned by ReadCSV
-    var onyomiMap, kunyomiMap, kunyomiWithHiragana, kanjiMeanings = map[string][]rune
+    var onyomiMap, kunyomiMap, kunyomiWithHiragana, kanjiMeanings map[string][]rune
 
     // Create  string array of filepaths, this will help us save into proper map
     filePaths := []string {
-        "./resources/KanjiFrequencyListOnyomi.csv"
-        "./resources/KanjiFrequencyListKunyomi.csv"
-        "./resources/KunyomiWithHiragana.csv"
-        "./resources/KanjiMeanings.csv"
+        "./resources/KanjiFrequencyListOnyomi.csv",
+        "./resources/KanjiFrequencyListKunyomi.csv",
+        "./resources/KunyomiWithHiragana.csv",
+        "./resources/KanjiMeanings.csv",
     }
 
-    lenFiles = len(filepaths)
+    lenFiles := len(filePaths)
 
     wg.Add(lenFiles)
+
+    // Iterate through all four
+    for _, filePath := range filePaths {
+        go func(filePath string) {
+            defer wg.Done()
+            csvMap, err := ReadCSV(filePath)
+            
+            if err != nil {
+                handleError(err, "File Wrong: " + filePath)
+                return
+            }
+            switch filePath {
+            case "./resources/KanjiFrequencyListOnyomi.csv":
+                onyomiMap = csvMap
+
+            case "./resources/KanjiFrequencyListKunyomi.csv": 
+                kunyomiMap = csvMap
+
+            case "./resources/KunyomiWithHiragana.csv":
+                kunyomiWithHiragana = csvMap
+
+            case "./resources/KanjiMeanings.csv":
+                kanjiMeanings = csvMap
+            }
+
+        }(filePath)
+    }
+
+    wg.Wait()
 
     //handleError(err, "csv_as_onyomi_map")
     //handleError(err, "csv_as_kunyomi_map")
     //handleError(err, "csv_as_kunyomi_hiragana_map")
-    //handleError(err, "csv_as_definitions")
+    //handleError(err, "kanjiMeanings")
+
+    
 
     // Loop to keep the program running unless the user types in "exit"
     for {
@@ -92,23 +124,18 @@ func main() {
             fmt.Println("Exiting the program...")
             break
         }
-        
-        // Use the input put in by the user to do checks to see if the value exists
-        onyomi_result := csv_as_onyomi_map[userInput]
-        kunyomi_result := csv_as_kunyomi_map[userInput]
-        kunyomi_hiragana_result := csv_as_kunyomi_hiragana_map[userInput]
-
+       
         // Send each string into the printMap
-        if onyomi_result != nil {
-            printMap("Onyomi", onyomi_result, userInput, &csv_as_definitions)
+        if onyomiMap != nil {
+            printMap("Onyomi", onyomiMap[userInput], userInput, &kanjiMeanings)
         }
 
-        if kunyomi_result != nil {
-            printMap("Kunyomi", kunyomi_result, userInput, &csv_as_definitions)
+        if kunyomiMap != nil {
+            printMap("Kunyomi", kunyomiMap[userInput], userInput, &kanjiMeanings)
         }
 
-        if kunyomi_hiragana_result != nil {
-            printMap("Kunyomi with Hiragana", kunyomi_hiragana_result, userInput, &csv_as_definitions)
+        if kunyomiWithHiragana != nil {
+            printMap("Kunyomi with Hiragana", kunyomiWithHiragana[userInput], userInput, &kanjiMeanings)
         }
 
         fmt.Println("Press enter to continue...")
